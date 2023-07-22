a4745ee8bd4d0c0407180b08015d47a1f13fb063
m5271033 <m5271033@u-aizu.ac.jp>
2023-07-22 18:21:18 +0900

    Create Blog App

--- Blog/settings.py
+++ Blog/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'BlogApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'BlogApp.User'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MESSAGE_TAGS = {
+    messages.INFO: 'alert alert-info',
+    messages.SUCCESS: 'alert alert-success',
+    messages.WARNING: 'alert alert-warning',
+    messages.ERROR: 'alert alert-danger',
+}
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'post-list'
+LOGOUT_REDIRECT_URL = 'post-list'
+
---
+++ BlogApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from BlogApp import views
+
+urlpatterns = [
+    path('post/list', views.PostListView.as_view(), name="post-list"),
+    path('post/search', views.PostSearchView.as_view(), name="post-search"),
+    path('post/my-list', views.PostMyListView.as_view(), name="post-my-list"),
+    path('post/bookmark-list', views.PostBookmarkListView.as_view(), name="post-bookmark-list"),
+    path('post/create', views.PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name="post-delete"),
+    path('post/<int:post_id>/bookmark/create', views.create_bookmark, name="post-bookmark-create"),
+    path('post/<int:post_id>/bookmark/delete', views.delete_bookmark, name="post-bookmark-delete"),
+    path('user/list', views.UserListView.as_view(), name="user-list"),
+    path('user/signup', views.SignUpView.as_view(), name="signup"),
+    path('user/login', views.BlogLoginView.as_view(), name="login"),
+    path('user/logout', views.BlogLogoutView.as_view(), name="logout"),
+]
